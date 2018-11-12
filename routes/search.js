ff414458fbf27aaaefa6b587dc140d5026056576
evukipm <eva.p.mejias@gmail.com>
2018-11-12 10:37:56 +0100

    search commented

--- routes/search.js
+++ routes/search.js
@@ -7,8 +7,16 @@ const Post = require('../models/post');
 
 
 router.get('/', (req, res, next) => {
+  // recoge las dos querys, tipo y búsqueda
   const searchType = req.query.type;
   const searchValue = req.query.class;
+
+  // según el tipo, user, título o descripcion, realiza una u otra petición
+  // a la base de datos.
+  // el $regex devuelve match si incluye la cadena a buscar
+  // sin necesidad de tener que poner en la búsqueda el título completo
+  // Título del post: Tortilla de patatas, búsqueda: illa
+  // daría true
 
   if (searchType === 'user') {
     User.find({ username: { $regex: searchValue, $options: 'i' } })
